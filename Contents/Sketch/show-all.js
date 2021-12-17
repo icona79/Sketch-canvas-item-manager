22051cad6d44b97d218640e0d42a61f285986e0b
Matteo Gratton <icona79@gmail.com>
2021-12-17 13:26:42 +0000

    Plugin created

---
+++ Contents/Sketch/show-all.js
@@ -0,0 +1,12 @@
+var onRun = function(context) {
+    const sketch = require("sketch");
+    const doc = sketch.getSelectedDocument();
+
+    doc.pages.forEach((page) => {
+        page.layers.forEach((layer) => {
+            if (layer.type !== "Artboard" && layer.type !== "SymbolMaster") {
+                layer.hidden = false;
+            }
+        });
+    });
+};
